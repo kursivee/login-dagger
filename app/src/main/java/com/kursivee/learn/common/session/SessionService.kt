7e5773df039808ba40b85e9b3970e97c4006cc3d
kursivee <kursivee.dev@gmail.com>
2018-10-21 19:08:55 -0400

    Removed user data injection

--- app/src/main/java/com/kursivee/learn/common/session/SessionService.kt
+++ app/src/main/java/com/kursivee/learn/common/session/SessionService.kt
@@ -4,7 +4,9 @@ import com.kursivee.learn.common.session.di.SessionScoped
 import javax.inject.Inject
 
 @SessionScoped
-data class SessionService @Inject constructor(var userData: UserData) {
+class SessionService @Inject constructor() {
+    var userData: UserData = UserData()
+
     fun start(name: String, token: String) {
         userData.name = name
         userData.token = token
--- app/src/main/java/com/kursivee/learn/common/session/UserData.kt
+++ app/src/main/java/com/kursivee/learn/common/session/UserData.kt
@@ -1,16 +1,6 @@
 package com.kursivee.learn.common.session
 
-import com.kursivee.learn.common.session.di.SessionScoped
-import javax.inject.Inject
-
-// Thinking about just removing data class all together
-// Not sure of a scenario where this would be useful
-
-@SessionScoped
-class UserData @Inject constructor() {
-    lateinit var name: String
-    lateinit var token: String
-
+data class UserData  constructor(var name: String =  "", var token: String = "") {
     fun clear() {
         name = ""
         token = ""
